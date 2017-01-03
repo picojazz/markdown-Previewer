3fb923e570fd35599abab14448d5e2c5d60accee
picojazz <picojazzz@gmail.com>
2017-01-03 21:56:00 +0000

    use showndown

---
+++ app.js
@@ -0,0 +1,19 @@
+var vm = new Vue({
+
+	el: "#mark",
+	data :{
+		text:"Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Herman Fassett](https://freecodecamp.com/hermanfassett)*"
+		
+		
+	},
+	computed : {
+		textMark: function () {
+			var converter = new showdown.Converter();
+            return converter.makeHtml(this.text);
+		}
+
+		}
+	
+
+
+})
